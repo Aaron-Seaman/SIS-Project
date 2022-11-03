44f77e8ecc1a66f3b02678d0c0c89f30d69d59ea
aaron.seaman <aaron.seaman@MU-C1-02.mullen.pvt.k12.co.us>
2022-11-03 13:12:27 -0600

    Word

--- SIS Project/src/main.java
+++ SIS Project/src/main.java
@@ -6,7 +6,7 @@ public class main
 			{
 				System.out.println("Hello World");
 				
-				System.out.println("Worddddddddd");
+				System.out.println("Worddddddddddd");
 
 				
 				System.out.println("Yo");
