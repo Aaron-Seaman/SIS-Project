0714bcdb66a0ccf9ce1e898df70cfa2ba6cc6fef
aaron.seaman <aaron.seaman@MU-C1-02.mullen.pvt.k12.co.us>
2022-11-09 10:04:41 -0700

    Partially completed Change method

--- SIS Project/src/Change.java
+++ SIS Project/src/Change.java
@@ -2,32 +2,89 @@ import java.util.Scanner;
 
 public class Change 
 {
-	public static void main(String[] args)
+
+	public static void main(String[] args) 
 	{
-		Scanner name = new Scanner(System.in);
+		Scanner change = new Scanner(System.in);
 		
-		System.out.println("Add or Delete a Student");
-		System.out.println("    (1) Add a Student");
-		System.out.println("    (2) Delete a Student");
+		System.out.println("Change Student Grades/Schedule");
+		System.out.println("    (1) Change Grade");
+		System.out.println("    (2) Switch Classes");
 		System.out.println("    (3) Return to the Main Menu");
 		
-		int input = name.nextInt();
+		int number = change.nextInt();
 		
-		if(input == 1)
+		if(number == 1)
 		{
-			System.out.println("What is the first and last name of the student you would like to add?");
-			String wholeName = name.nextLine();
+			System.out.println("What is the first name of the student whose grade you would like to change?");
+			String grade = change.nextLine();
+			if(grade.equals(studentNamesFile))
+			{
+				System.out.println("Which class would you like to switch your grade in?");
+				String classy = change.nextLine();
+				
+				if(classy.equals(studentNamesFile))
+				{
+					System.out.println("What would you like the current grade to be changed to?");
+					String newGrade = change.nextLine();
+
+					for(int i = 0; i<studentNamesFile.length; i++)
+					{
+						if(newGrade.equals(StudentNamesFile))
+						{
+							//studentNamesFile.setGrade(i);
+						}
+					}
+				}
+				else
+				{
+					System.out.println("That is not a student.");
+				}
+			}
+			else
+			{
+				System.out.println("That is not one of the options.");
+			}
 		}
-		else if(input == 2)
+		else if(number == 2)
 		{
-			System.out.println("What is the first name of the student you would like to delete?");
-			String wholeName = name.nextLine();
+			System.out.println("What is the first name of the student whose class you would like to change?");
+			String student = change.nextLine();
+			
+			if(student.equals(studentNamesFile))
+			{
+				System.out.println("What class would you like to change?");
+				String classy = change.nextLine();
+				
+				if(classy.equals(studentNamesFile))
+				{
+					System.out.println("What is the new class you would like to be put into?");
+					String newClass = change.nextLine();
+					
+					for(int i = 0; i<studentNamesFile.length; i++)
+					{
+						if(newClass.equals(StudentNamesFile))
+						{
+							//studentNamesFile.setClass(i)
+						}
+					}
+				}
+				else
+				{
+					System.out.println("That is not one of the options.");
+				}
+			}
+			else
+			{
+				System.out.println("That is not a student.");
+			}
 		}
 		else
 		{
 			//return them to the main menu
 		}
-			
+		
+
 	}
-	
+
 }
